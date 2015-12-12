e19f2f1fe80a92a44baa39fc233a4b000e0051e7
medyakovvit <medyakovvit@gmail.com>
2015-12-12 14:53:56 +0400

    Initial commit

---
+++ mymodel.cpp
@@ -0,0 +1,7 @@
+#include "mymodel.h"
+
+MyModel::MyModel()
+{
+
+}
+
---
+++ mymodel.h
@@ -0,0 +1,12 @@
+#ifndef MYMODEL_H
+#define MYMODEL_H
+
+#include <QObject>
+
+class MyModel : public QAbstractListModel
+{
+public:
+    MyModel();
+};
+
+#endif // MYMODEL_H

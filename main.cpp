dd804b8b4995d3a5ed5889d76e57ee2ec554986f
medyakovvit <medyakovvit@gmail.com>
2015-12-12 15:08:25 +0400

    Propogate MyModel object to qml
    via setContextProperty()

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QtQml>
+
+#include "mymodel.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QApplication app(argc, argv);    
 
     QQmlApplicationEngine engine;
+    MyModel model;
+    engine.rootContext()->setContextProperty("myModel", &model);
+
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();

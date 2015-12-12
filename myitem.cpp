e8644f6adf0703a499c5c811ddb2406fc92a2056
medyakovvit <medyakovvit@gmail.com>
2015-12-12 15:05:54 +0400

    Create class MyItem to be stored in MyModel

--- myitem.cpp
+++ myitem.cpp
@@ -1,7 +1,53 @@
 #include "myitem.h"
 
-MyItem::MyItem(QObject *parent) : QObject(parent)
+MyItem::MyItem(int aId, int aX, int aY, QString aName, QColor aColor, QObject *parent) : QObject(parent),
+    m_id(aId), m_x(aX), m_y(aY), m_name(aName), m_color(aColor)
 {
 
+}
+
+void MyItem::setId(int id)
+{
+    if (m_id == id)
+        return;
+
+    m_id = id;
+    emit idChanged(id);
+}
+
+void MyItem::setX(int x)
+{
+    if (m_x == x)
+        return;
+
+    m_x = x;
+    emit xChanged(x);
+}
+
+void MyItem::setY(int y)
+{
+    if (m_y == y)
+        return;
+
+    m_y = y;
+    emit yChanged(y);
+}
+
+void MyItem::setName(QString name)
+{
+    if (m_name == name)
+        return;
+
+    m_name = name;
+    emit nameChanged(name);
+}
+
+void MyItem::setColor(QColor color)
+{
+    if (m_color == color)
+        return;
+
+    m_color = color;
+    emit colorChanged(color);
 }
 
--- myitem.h
+++ myitem.h
@@ -2,16 +2,46 @@
 #define MYITEM_H
 
 #include <QObject>
+#include <QColor>
 
 class MyItem : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(int myId READ id WRITE setId NOTIFY idChanged)
+    Q_PROPERTY(int x READ x WRITE setX NOTIFY xChanged)
+    Q_PROPERTY(int y READ y WRITE setY NOTIFY yChanged)
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY colorChanged)
+
 public:
-    explicit MyItem(QObject *parent = 0);
+    explicit MyItem(int aId, int aX, int aY, QString aName, QColor aColor, QObject *parent = 0);
+
+    int id() const{return m_id;}
+    int x() const{return m_x;}
+    int y() const{return m_y;}
+    QString name() const{return m_name;}
+    QColor color() const{return m_color;}
 
 signals:
+    void idChanged(int id);
+    void xChanged(int x);
+    void yChanged(int y);
+    void nameChanged(QString name);
+    void colorChanged(QColor color);
 
 public slots:
+    void setId(int id);
+    void setX(int x);
+    void setY(int y);
+    void setName(QString name);
+    void setColor(QColor color);
+
+protected:
+    int m_id;
+    int m_x;
+    int m_y;
+    QString m_name;
+    QColor m_color;
 };
 
 #endif // MYITEM_H
